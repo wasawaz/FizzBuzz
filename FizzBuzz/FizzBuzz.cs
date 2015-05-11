c0fc2d687813f041c7dedf2c4fa58de502f723d2
Wasawat <wasawaz@gmail.com>
2015-05-11 18:02:13 +0700

    First commit

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzzLibrary
+{
+    public class FizzBuzz
+    {
+        private int FizzNumber = 3;
+        private int BuzzNumber = 5;
+
+        public String Count(int countInput)
+        {
+            string countString = string.Empty;
+            if (IsFizzNumber(countInput) && IsBuzzNumber(countInput))
+            {
+                countString = "FizzBuzz";
+            }
+            else if (IsFizzNumber(countInput))
+            {
+                countString = "Fizz";
+            }
+            else if (IsBuzzNumber(countInput))
+            {
+                countString = "Buzz";
+            }
+            else
+            {
+                countString = countInput.ToString();
+            }
+            return countString;
+        }
+
+        private bool IsBuzzNumber(int countInput)
+        {
+            return countInput % BuzzNumber == 0;
+        }
+
+        private bool IsFizzNumber(int countInput)
+        {
+            return countInput % FizzNumber == 0;
+        }
+    }
+}
---
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using FizzBuzzLibrary;
+using NUnit.Framework;
+
+namespace FizzBuzzTest
+{
+    [TestFixture]
+    public class FizzBuzzTest
+    {
+        [Test]
+        public void Count_Shouldbe1_WhenInput1()
+        {
+            FizzBuzz fizzbuzz = new FizzBuzz();
+            Assert.AreEqual("1", fizzbuzz.Count(1));
+        }
+
+
+        [Test]
+        public void Count_Shouldbe2_WhenInput2()
+        {
+            FizzBuzz fizzbuzz = new FizzBuzz();
+            Assert.AreEqual("2", fizzbuzz.Count(2));
+        }
+
+
+        [Test]
+        public void Count_ShouldbeFizz_WhenInput3()
+        {
+            FizzBuzz fizzbuzz = new FizzBuzz();
+            Assert.AreEqual("Fizz", fizzbuzz.Count(3));
+        }
+
+        [Test]
+        public void Count_Shouldbe4_WhenInput4()
+        {
+            FizzBuzz fizzbuzz = new FizzBuzz();
+            Assert.AreEqual("4", fizzbuzz.Count(4));
+        }
+
+        [Test]
+        public void Count_ShouldbeBuzz_WhenInput5()
+        {
+            FizzBuzz fizzbuzz = new FizzBuzz();
+            Assert.AreEqual("Buzz", fizzbuzz.Count(5));
+        }
+
+        [Test]
+        public void Count_ShouldbeFizz_WhenInput6()
+        {
+            FizzBuzz fizzbuzz = new FizzBuzz();
+            Assert.AreEqual("Fizz", fizzbuzz.Count(6));
+        }
+
+        [Test]
+        public void Count_ShouldbeFizz_WhenInput9()
+        {
+            FizzBuzz fizzbuzz = new FizzBuzz();
+            Assert.AreEqual("Fizz", fizzbuzz.Count(9));
+        }
+
+        [Test]
+        public void Count_ShouldbeBuzz_WhenInput10()
+        {
+            FizzBuzz fizzbuzz = new FizzBuzz();
+            Assert.AreEqual("Buzz", fizzbuzz.Count(10));
+        }
+
+        [Test]
+        public void Count_ShouldbeFizzBuzz_WhenInput15()
+        {
+            FizzBuzz fizzbuzz = new FizzBuzz();
+            Assert.AreEqual("FizzBuzz", fizzbuzz.Count(15));
+        }
+
+    }
+}
